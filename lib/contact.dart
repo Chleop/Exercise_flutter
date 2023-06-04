9a9b62e8cd2147d1a3d51f215c2c78f56d6a286f
chleo <chleoproeschel@hotmail.fr>
2023-06-04 20:00:14 +0200

    simplified body and added Arielle D

--- lib/contact.dart
+++ lib/contact.dart
@@ -4,26 +4,34 @@ class ContactPage extends StatelessWidget {
 
   final Map<String, String> contacts = {
   'Proeschel Chleo': '06 16 40 72 53',
-  'Proesche Chle':'06 16 40 72 5', 
-  'Proesch Chl':'06 16 40 72',
-  'Proesc Ch':'06 16 40 7',
-  'Proesc C':'06 16 40',
-  'Proesc':'06 16 40',
-  'Proes':'06 16 4',
-  'Proe':'06 16',
-  'Pro':'06 1',
-  'Pr':'06',
-  'P':'0',
+  'Proesche Chle':'06 16 40 72 53', 
+  'Proesch Chl':'06 16 40 72 53',
+  'Proesc Ch':'06 16 40 72 53',
+  'Proesc C':'06 16 40 72 53',
+  'Proesc':'06 16 40 72 53',
+  'Proes':'06 16 40 72 53',
+  'Proe':'06 16 40 72 53',
+  'Pro':'06 16 40 72 53',
+  'Pr':'06 16 40 72 53',
+  'P':'06 16 40 72 53',
   'Johnny Hallyday':'06 66 66 66 66',
-  'Johnn Hallyda':'06 66 66 66 6',
-  'John Hallyd':'06 66 66 66',
-  'Joh Hally':'06 66 66 6',
-  'Jo Hally':'06 66 66',
-  'J Hall':'06 66 6',
-  'Hall':'06 66',
-  'Hal':'06 6',
-  'Ha':'06',
-  'H':'0',
+  'Johnn Hallyda':'06 66 66 66 66',
+  'John Hallyd':'06 66 66 66 66',
+  'Joh Hally':'06 66 66 66 66',
+  'Jo Hally':'06 66 66 66 66',
+  'J Hall':'06 66 66 66 66',
+  'Hall':'06 66 66 66 66',
+  'Hal':'06 66 66 66 66',
+  'Ha':'06 66 66 66 66',
+  'H':'06 66 66 66 66',
+  'Arielle Dombasle':'06 66 66 66 66',
+  'Ariell Dombasl':'06 66 66 66 66',
+  'Ariel Dombas':'06 66 66 66',
+  'Arie Domba':'06 66 66 66 66',
+  'Ari Domb':'06 66 66 66 66',
+  'Ar Dom':'06 66 66 66 66',
+  'A Do':'06 66 66 66 66',
+  'D':'06 66 66 66 66',
   };
   
   @override
@@ -38,27 +46,22 @@ class ContactPage extends StatelessWidget {
         alignment: Alignment.center,
         child: ListView.builder(
           itemCount: contacts.length,
-          itemBuilder: (context, index) {
+          itemBuilder: (BuildContext context, int index) {
             final contactName = contacts.keys.elementAt(index);
             final contactNumber = contacts.values.elementAt(index);
+            
             return Column(
               children: [
                 Text(
                   contactName,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 18),
                 ),
                 Text(
                   contactNumber,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  style: TextStyle(fontSize: 14),
                 ),
-                SizedBox(height: 16),
-              ],
+
+              ]
             );
           },
         ),
